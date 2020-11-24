34c63849d9e0028465d50f703bfc1e308221f202
Patrick Robinson <patrob@gmail.com>
2020-11-24 13:50:44 -0600

    updated email link

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,16 +4,17 @@ import { SocialIcon } from 'react-social-icons';
 
 class Footer extends Component {
     render() {
+        let iconStyle = {height: 50, width: 50};
         return (
             <Navbar className="footer align-middle" color="light" light expand="md">
                 <Container className="justify-content-center">
                     <Row className="text-center">
                         <Col xs="12" sm="12" md="12" lg="12" xl="12">
-                            <SocialIcon className="mx-2" url="http://twitter.com/realPatRobinson" style={{ height: 50, width: 50 }} bgColor="#333333" />
-                            <SocialIcon className="mx-2" url="http://facebook.com/patrobsatx" style={{ height: 50, width: 50 }} bgColor="#333333" />
-                            <SocialIcon className="mx-2" url="http://instagram.com/patrobsatx" style={{ height: 50, width: 50 }} bgColor="#333333" />
-                            <SocialIcon className="mx-2" url="http://linkedin.com/in/patrick-robinson-b8b6628" style={{ height: 50, width: 50 }} bgColor="#333333" />
-                            <SocialIcon className="mx-2" url="mailto:patrob@gmail.com" style={{ height: 50, width: 50 }} bgColor="#333333" />
+                            <SocialIcon className="mx-2" url="http://twitter.com/realPatRobinson" style={iconStyle} bgColor="#333333" />
+                            <SocialIcon className="mx-2" url="http://facebook.com/patrobsatx" style={iconStyle} bgColor="#333333" />
+                            <SocialIcon className="mx-2" url="http://instagram.com/patrobsatx" style={iconStyle} bgColor="#333333" />
+                            <SocialIcon className="mx-2" url="http://linkedin.com/in/patrick-robinson-b8b6628" style={iconStyle} bgColor="#333333" />
+                            <SocialIcon className="mx-2" url="mailto:me@patrickarobinson.com" style={iconStyle} bgColor="#333333" />
                         </Col>
                     </Row>
                 </Container>
